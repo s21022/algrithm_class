0b884873d5d2b676d01d0389d43bac4664760219
s21022 <s21022@std.it-college.ac.jp>
2023-05-25 12:21:59 +0900

    EX.py 修正

--- Chapter3/EX.py
+++ Chapter3/EX.py
@@ -1,14 +1,5 @@
-import queue
-
 Max = 7
 node = ["赤", "橙", "黄", "緑", "青", "藍", "紫"]
 
-# print(node)
-# q = queue.Queue()
-# for i in range(Max):
-#     print(node[i], end="→")
-# print("\n")
-
 print("→".join(node))
 print("→".join(node[::-1]))
-    #print(i, end="→")
