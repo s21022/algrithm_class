5fe565193189b08d4599b04f7b12eeae787d106a
s21022 <s21022@std.it-college.ac.jp>
2023-06-22 10:05:35 +0900

    task.py dijkstras_algorithm_3.py 追加

--- Chapter7/task.py
+++ Chapter7/task.py
@@ -5,16 +5,16 @@ def euclid(a, b):
         return euclid(b, a % b)
 
 
-print("a≧bとなる自然数を入力してください")
+print("a以下の自然数を入力してください")
 a = int(input("a="))
 b = int(input("b="))
 c = int(input("c="))
 if b == 0:  # 0のときエラーを出す
-    print("0はだめです")
+    print("0以外の数字を指定してください")
     print("もう一度入力してください")
     b = int(input("b="))
 if c == 0:  # 0のときエラーを出す
-    print("0はだめです")
+    print("0以外の数字を指定してください")
     print("もう一度入力してください")
     c = int(input("c="))
 
@@ -28,4 +28,9 @@ def euclid2(ab, c):
         return euclid2(c, ab % c)
 
 
+def lcm(a, b, c):
+    return a * b * c / euclid2(ab, c)
+
+
 print("それらの数の最大公約数は", euclid2(ab, c))
+print("それらの自然数の最小公倍数は", lcm(a, b, c))
